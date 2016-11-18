330edddd927397e1289c187572bb54b1cd21a01d
mike-schlegel <schlegelnc@gmail.com>
2016-11-18 14:06:54 -0500

    UserController Changes

---
+++ TourneyMaker/TourneyMaker/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TourneyMaker.Models;
+using Newtonsoft.Json;
+
+namespace TourneyMaker.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public string RegisterUser(string _data)
+        {
+            UserManager um = new UserManager();
+            UserInfo data = JsonConvert.DeserializeObject<UserInfo>(_data);
+            if(um.Register(data.username, data.password, data.email))
+            {
+                return JsonConvert.SerializeObject(um);
+            }
+            else
+            {
+                return "error";
+            }
+        }
+
+        public string LoginUser(string _data)
+        {
+            UserManager um = new UserManager();
+            UserInfo data = JsonConvert.DeserializeObject<UserInfo>(_data);
+            um.Login(data.username, data.password);
+            return JsonConvert.SerializeObject(um);
+        }
+
+    }
+}
