9bef3b35180e842db0af60fd53903589e99172ff
Michael Vogelsang <mvogelsang@rocketmail.com>
2016-11-18 16:33:09 -0500

    initialize unit testing

--- TourneyMaker/TourneyMaker/app/dashboard/profile/profile.controller.js
+++ TourneyMaker/TourneyMaker/app/dashboard/profile/profile.controller.js
@@ -22,3 +22,4 @@ var TourneyMaker;
     TourneyMaker.ProfileController = ProfileController;
     TourneyMaker.app.controller('ProfileController', ProfileController);
 })(TourneyMaker || (TourneyMaker = {}));
+//# sourceMappingURL=profile.controller.js.map
--- TourneyMaker/TourneyMaker/app/landing-page/landing-page.controller.js
+++ TourneyMaker/TourneyMaker/app/landing-page/landing-page.controller.js
@@ -102,3 +102,4 @@ var TourneyMaker;
     TourneyMaker.LandingPageController = LandingPageController;
     TourneyMaker.app.controller("LandingPageController", LandingPageController);
 })(TourneyMaker || (TourneyMaker = {}));
+//# sourceMappingURL=landing-page.controller.js.map
