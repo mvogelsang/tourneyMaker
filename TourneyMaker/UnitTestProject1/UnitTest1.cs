47c64e8f6bbfdda9c8fa145d0d4775811b9b3f1e
James D Lutes <jdlute02@gmail.com>
2016-12-05 16:46:16 -0500

    jay's test

--- TourneyMaker/UnitTestProject1/UnitTest1.cs
+++ TourneyMaker/UnitTestProject1/UnitTest1.cs
@@ -1,17 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TourneyMaker;
+using TourneyMaker.Controllers;
+using TourneyMaker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-
-namespace UnitTestProject1
+namespace UnitTesting
 {
     [TestClass]
-    public class UnitTest1
+    public class UserControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UserRegisterTest()
         {
+            bool registered = false;
+            UserInfo TestUser = new UserInfo();
+            TestUser.email = "test@test.com";
+            TestUser.password = "test123";
+            TestUser.username = "testUser123";
+
+            UserManager TestUserManager = new UserManager();
+            registered = TestUserManager.Register(TestUser.username, TestUser.password, TestUser.email);
+            Assert.IsTrue(registered == true);
+        }
+
+        [TestMethod]
+        public void UserLoginTest()
+        {
+            string TestUserName = "testuser123";
+            string TestUserPass = "test123";
+
+            UserManager TestUserManager = new UserManager();
+            TestUserManager.Login(TestUserName, TestUserPass);
+
+            // Assert.AreEqual(TestUserManager.authorized == true);
 
         }
     }
