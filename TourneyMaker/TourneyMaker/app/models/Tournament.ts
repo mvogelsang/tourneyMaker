60b94765f1ee9cb8b6513cd155e7c00e40aa190d
Travis Klipa <Travis Klipa@TRAVISKLIPA>
2016-12-05 14:22:32 -0500

    Model change

--- TourneyMaker/TourneyMaker/app/models/Tournament.ts
+++ TourneyMaker/TourneyMaker/app/models/Tournament.ts
@@ -1,13 +1,13 @@
 ﻿module TourneyMaker {
     export class Tournament {
-        public tid: number;
-        public tname: string;
-        public numParticipants: number;
-        public host: User;
-        public managers: Array<User>;
-        public participants: Array<User>;
-        public rounds;
-        public commaDlParts: Array<string>;
+        tid: number;
+        tname: string;
+        numParticipants: number;
+        host: User;
+        managers: Array<User>;
+        participants: Array<User>;
+        rounds;
+        commaDlParts: Array<string>;
 
     }
 }
