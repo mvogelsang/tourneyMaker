87c9f7ba11e5c73b85c8422645dd849a41ef3cf3
Travis Klipa <Travis Klipa@TRAVISKLIPA>
2016-12-06 10:40:37 -0500

    Added IDs to form elements for testing. Changed UI a bit.

--- TourneyMaker/TourneyMaker/app/dashboard/profile/profile.controller.js
+++ TourneyMaker/TourneyMaker/app/dashboard/profile/profile.controller.js
@@ -36,4 +36,3 @@ var TourneyMaker;
     TourneyMaker.ProfileController = ProfileController;
     TourneyMaker.app.controller('ProfileController', ProfileController);
 })(TourneyMaker || (TourneyMaker = {}));
-//# sourceMappingURL=profile.controller.js.map
