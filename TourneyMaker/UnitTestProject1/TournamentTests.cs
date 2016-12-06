947cd4d21ae40b0217be6f16df67e411eab2d43e
Michael Vogelsang <mvogelsang@rocketmail.com>
2016-12-06 11:25:45 -0500

    set up groundwork further, get integrations set up

--- TourneyMaker/UnitTestProject1/TournamentTests.cs
+++ TourneyMaker/UnitTestProject1/TournamentTests.cs
@@ -1,14 +1,26 @@
 ﻿using System;
+using TourneyMaker;
+using TourneyMaker.Controllers;
+using TourneyMaker.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public static string GetRandomAlphaNumeric()
         {
+            Random random = new Random();
+            var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(chars.Select(c => chars[random.Next(chars.Length)]).Take(8).ToArray());
+        }
+
+        [TestMethod]
+        public void blankTest()
+        {
+            Assert.AreEqual(1, 0);
         }
     }
 }
