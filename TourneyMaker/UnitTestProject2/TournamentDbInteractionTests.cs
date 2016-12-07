0f87a0b34eca8e53d542900ce65e43d9ff79179b
Michael Vogelsang <mvogelsang@rocketmail.com>
2016-12-06 19:59:37 -0500

    finish unit and integration tests

--- TourneyMaker/UnitTestProject2/TournamentDbInteractionTests.cs
+++ TourneyMaker/UnitTestProject2/TournamentDbInteractionTests.cs
@@ -20,7 +20,17 @@ namespace UnitTestProject2
         [TestMethod]
         public void TournamentDbIntegration()
         {
-            Assert.AreEqual(1, 0);
+            Tournament t = new Tournament();
+            Tournament res = new Tournament();
+            t.host.username = "testname";
+            t.host.email = "testuser@test.com";
+            t.numParticipants = 4;
+            t.tname = GetRandomAlphaNumeric();
+            t.commaDlParts = "1,2,3,4";
+            TourneyManager test = new TourneyManager();
+            res = test.CreateNewTourney(t);
+
+            Assert.IsTrue(test.GetTournament(res.tid).tname == t.tname);
         }
     }
 }
