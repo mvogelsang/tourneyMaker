221b8d906f5194905761936fadab150706f05a48
Travis Klipa <Travis Klipa@TRAVISKLIPA>
2016-12-07 16:51:56 -0500

    Fixe again

--- TourneyMaker/TourneyMaker/app/dashboard/view-tournament/view-tournament.controller.js
+++ TourneyMaker/TourneyMaker/app/dashboard/view-tournament/view-tournament.controller.js
@@ -114,4 +114,3 @@ var TourneyMaker;
     TourneyMaker.ViewTournamentController = ViewTournamentController;
     TourneyMaker.app.controller("ViewTournamentController", ViewTournamentController);
 })(TourneyMaker || (TourneyMaker = {}));
-//# sourceMappingURL=view-tournament.controller.js.map
